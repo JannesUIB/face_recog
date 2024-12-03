b7965d800532fa407fdd6d98855695ac3d76b53b
Jannes <jenesverlando@gmail.com>
2024-12-03 21:28:55 +0700

    add new page, fix styling, more functions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify,redirect, url_for, session
 import base64
+import psycopg2
 import numpy as np
 from PIL import Image
 from io import BytesIO
@@ -7,8 +8,27 @@ from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 import pickle
 import cv2
-app = Flask(__name__)
+import math
+from datetime import datetime, timedelta, time
+from flask_bcrypt import Bcrypt
+import bcrypt
+from collections import Counter
 
+app = Flask(__name__)
+app.secret_key = 'uib_ai_recog'  # Use a strong secret key
+# bcrypt = Bcrypt(app)
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='ai_recog_db',
+                            user="postgres",
+                            password="postgres")
+    return conn
+
+UIB_LAT = 1.1208372285337016
+UIB_LONG = 104.00328410332459
+
+waktu_masuk = time(8,0)
+waktu_pulang = time(17,0)
 # Load your trained model
 model = load_model("face_recognition_model_4.keras")
 
@@ -16,6 +36,30 @@ with open("label_encoder_2.pkl", "rb") as f:
     label_encoder = pickle.load(f)
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    """
+    Calculate the great-circle distance between two points
+    on the Earth using the Haversine formula.
+    """
+    # Radius of the Earth in meters
+    R = 6371000
+
+    # Convert latitude and longitude from degrees to radians
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    delta_phi = math.radians(lat2 - lat1)
+    delta_lambda = math.radians(lon2 - lon1)
+
+    # Haversine formula
+    a = math.sin(delta_phi / 2.0) ** 2 + \
+        math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda / 2.0) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    # Distance in meters
+    distance = R * c
+    return distance
 
 def preprocess_image(image):
     """
@@ -49,18 +93,6 @@ def preprocess_image(image):
     plt.show()
     return face_batch
 
-# def preprocess_image(image):
-#     """
-#     Preprocess the image for the model.
-#     - Resize, normalize, etc.
-#     """
-#     image = image.resize((160, 160))  # Resize to match model input
-#     image = np.array(image) / 255.0  # Normalize pixel values
-#     image = np.expand_dims(image, axis=0)  # Add batch dimension
-
-
-#     return image
-
 def decode_base64_image(base64_string):
     """
     Decode a Base64 string to a PIL image.
@@ -77,40 +109,221 @@ def recognize_face(base64_image):
     # Decode Base64 to image
     image = decode_base64_image(base64_image)
 
-    print("the image", image)
-    
     # Preprocess the image
     processed_image = preprocess_image(image)
     
-    print("preprocessed image?", preprocess_image)
     # Predict using the model
     predictions = model.predict(processed_image)
 
-    print("the predicitions", predictions)
     predicted_class = np.argmax(predictions)  # Get the class ID
     
-    print("prediction_class?", predicted_class)
 
     # Convert index to label
     predicted_label = label_encoder.inverse_transform([predicted_class])[0]
 
-    print("the predicted label", predicted_label)
-    # Map class ID to a name or ID
-    # person_id = id_mapping.get(predicted_class, "Unknown")
-    return ":hehe"
+    return predicted_label
+
+
+# Route to render login page
+@app.route('/login', methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+# Route to handle login
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    passwords = request.form['password']
+
+    # Connect to the database
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Query the user
+    cur.execute("SELECT * FROM admin WHERE email = %s", (email,))
+    user = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    print("password", passwords.encode('utf-8'))
+    print("password", user[2].encode('utf-8'))
+    # Validate user credentials
+    if not user or not bcrypt.checkpw(passwords.encode('utf-8'), user[2].encode('utf-8')):
+        return jsonify({'error': 'Invalid Email or password'}), 401
+
+    # Save user session
+    session['user_id'] = user[0]
+    session['username'] = user[1]
+    session['email'] = user[3]
+    return redirect(url_for('index'))
+
+# Logout route
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+@app.route("/get_image_by_karyawan_id" , methods=['POST'])
+def get_image_karyawan():
+    data = request.get_json()
+    karyawan_id = data.get("id")
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM karyawan where npm = %s;", (karyawan_id,))
+    karyawan = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    image_base64 = base64.b64encode(karyawan[0][2].tobytes()).decode('utf-8')
+    
+    return jsonify({"image_base64" : image_base64})
+
+
+@app.route("/take_attendance" , methods=['POST'])
+def post_take_attendance():
+    data = request.get_json()
+    karyawan_id = data.get("id")
+    attendance_status = data.get("status")
+
+    now = datetime.now()
+
+    if attendance_status == "" or False:
+        return jsonify({'error' : "Attedance Status Is Invalid, Please Check With IT Team"})
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    if attendance_status == "Not Attended":
+        cur.execute("UPDATE karyawan set status = 'Attended' where npm = %s;", (karyawan_id,))
+        cur.execute("INSERT INTO absensi (karyawan_id, waktu_masuk, status) VALUES (%s,%s,%s);", (karyawan_id,now,'Masuk'))
+
+    elif attendance_status == "Attended":
+        cur.execute("UPDATE karyawan set status = 'Not Attended' where npm = %s;", (karyawan_id,))
+
+        if now.time() >= waktu_masuk and now.time() <= waktu_pulang:
+            cur.execute("INSERT INTO absensi (karyawan_id, waktu_keluar, status) VALUES (%s,%s,%s);", (karyawan_id,now,'Keluar'))
+        else:
+            cur.execute("INSERT INTO absensi (karyawan_id, waktu_keluar, status) VALUES (%s,%s,%s);", (karyawan_id,now,'Pulang'))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    # karyawan = cur.fetchall()
+    # image_base64 = base64.b64encode(karyawan[0][2].tobytes()).decode('utf-8')
+    
+    return jsonify({"sucesss" : True})
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    is_token = request.args.get("token") or False
+    is_admin = False
+    if session.get('user_id'):
+        is_admin = True
+
+    return render_template('index.html', is_admin=is_admin, is_token=is_token)
+
+@app.route('/report')
+def report():
+    is_admin = False
+    if session.get('user_id'):
+        is_admin = True
+
+    now = datetime.now()
+    start_time = datetime.combine(now.date(), datetime.min.time())
+    end_time = datetime.combine(now.date(), datetime.max.time())
+
+    # Get the search query from the request
+    search_query = request.args.get('search', '')
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    if search_query:
+        cur.execute("""
+            SELECT absensi.*, karyawan.nama FROM absensi
+            JOIN karyawan ON absensi.karyawan_id = karyawan.npm
+            WHERE (absensi.karyawan_id::text LIKE %s OR karyawan.nama LIKE %s)
+            AND (waktu_masuk >= %s OR waktu_keluar <= %s);
+        """, ('%' + search_query + '%', '%' + search_query + '%', start_time, end_time))
+    else:
+        cur.execute("""
+            SELECT absensi.*, karyawan.nama FROM absensi
+            JOIN karyawan ON absensi.karyawan_id = karyawan.npm
+            WHERE (waktu_masuk >= %s OR waktu_keluar <= %s);
+        """, (start_time, end_time))
+    absen_ids = cur.fetchall()
+    cur.close()
+    conn.close()
+    
+    statuses = [row[4].strip() for row in absen_ids]
+    status_counts = Counter(statuses)
+    return render_template('report.html', is_admin=is_admin, masuk_count=status_counts.get('Masuk', 0), keluar_count=status_counts.get('Keluar', 0),pulang_count=status_counts.get('Pulang', 0), data_absen = absen_ids)
+
+@app.route('/confirmation')
+def confirmation():
+    id = request.args.get("id")
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM karyawan where npm = %s;", (id,))
+    karyawan_id = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    is_admin = False
+    if session.get('user_id'):
+        is_admin = True
+    return render_template("absen_confirmation.html", id=id, name=karyawan_id[0][4], email=karyawan_id[0][3], status=karyawan_id[0][1], is_admin=is_admin)
+
 
 
 @app.route('/api/capture-face', methods=['POST'])
 def capture_face():
     data = request.get_json()
     base64_image = data.get("image")
+    lat = data.get("latitude")
+    long = data.get("longitude")
+
+    distance = haversine_distance(UIB_LAT, UIB_LONG, lat, long)
+
+    # Check if within 300 meters
+    if distance <= 300:
+        print(f"Within 300 meters. Distance: {distance:.2f} meters")
     
-    if not base64_image:
-        return jsonify({"error": "No image provided"}), 400
+        if not base64_image:
+            return jsonify({"error": "No image provided"}), 400
+        
+        person_id = recognize_face(base64_image)
+        return jsonify({"success" : True,"id": person_id})
+    else:
+        return jsonify({"error": "You Are Too Far"}), 400
+
+
+@app.route('/api/token_attendance', methods=['POST'])
+def token_attendance():
+    data = request.get_json()
+    token = data.get("token_input")
+    lat = data.get("latitude")
+    long = data.get("longitude")
+
+    distance = haversine_distance(UIB_LAT, UIB_LONG, lat, long)
+
+    # Check if within 300 meters
+    # if distance <= 300:
+    print(f"Within 300 meters. Distance: {distance:.2f} meters")
+
+    if not token:
+        return jsonify({"error": "No Token provided"}), 400
     
-    person_id = recognize_face(base64_image)
-    return jsonify({"id": person_id})
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM karyawan where token = %s;", (token,))
+    karyawan_id = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    if not karyawan_id:
+        return jsonify({"error": "No Data Found"}), 400
+    return jsonify({"success" : True,"id": karyawan_id[0][0]})
+    # else:
+    #     return jsonify({"error": "You Are Too Far"}), 400
---
+++ init_db.py
@@ -0,0 +1,140 @@
+import os
+import psycopg2
+from PIL import Image, ImageDraw, ImageFont
+from flask_bcrypt import Bcrypt
+import bcrypt
+import random
+import string
+
+# Path to save generated images
+output_dir = "/home/sgeede/Documents/generated_images/"
+
+# Create directory if it doesn't exist
+import os
+os.makedirs(output_dir, exist_ok=True)
+
+conn = psycopg2.connect(
+        host="localhost",
+        database="ai_recog_db",
+        user="postgres",
+        password="postgres")
+
+username = 'admin'  # Change as needed
+password = 'securepassword'  # Change as needed
+
+# Hash the password
+salt = bcrypt.gensalt()  # Generates a salt
+hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a command: this creates a new table
+cur.execute('DROP TABLE IF EXISTS karyawan CASCADE;')
+cur.execute('DROP TABLE IF EXISTS admin;')
+cur.execute('DROP TABLE IF EXISTS absensi;')
+
+cur.execute('CREATE TABLE karyawan (npm varchar(24) PRIMARY KEY,'
+            'status varchar(16) NOT NULL,'
+            'image BYTEA,'
+            'email varchar(150) NOT NULL,'
+            'nama varchar (150) NOT NULL,'
+            'token varchar (150) NOT NULL);'
+            )
+cur.execute('CREATE TABLE admin (id serial PRIMARY KEY,'
+            'nama varchar (150) NOT NULL,'
+            'password varchar (150) NOT NULL,'
+            'email varchar (150) NOT NULL);'
+            )
+cur.execute('CREATE TABLE absensi (id serial PRIMARY KEY,'
+            'karyawan_id varchar(24) NOT NULL,'
+            'waktu_masuk TIMESTAMP WITHOUT TIME ZONE,'
+            'waktu_keluar TIMESTAMP WITHOUT TIME ZONE,'
+            'status char(16) NOT NULL,'
+            'CONSTRAINT fk_karyawan FOREIGN KEY(karyawan_id) REFERENCES karyawan(npm));'
+            )
+
+data = [
+  ('1200005', 'Patrick Pratama Hendri', 'Not Attended', '1200005.patrick@email.com', 'zedkxdrnfplsuchj'),
+  ('5220015', 'Sandy Putra Efendi', 'Not Attended', '5220015.sandy@email.com', 'fnbxucrruuqsozmu'),
+  ('5220021', 'Sandy Alferro Dion', 'Not Attended', '5220021.sandy@email.com', 'ygebkxnuemntwhkf'),
+  ('7180066', 'Herman', 'Not Attended', '7180066.herman@email.com', 'hkcywyqiiujdfjnm'),
+  ('2231002', 'Jetset', 'Not Attended', '2231002.jetset@email.com', 'xdthygofxlxaqnew'),
+  ('2231061', 'Erwin', 'Not Attended', '2231061.erwin@email.com', 'ecdnjcjaicqdwqaj'),
+  ('2231064', 'Fernando Jose', 'Not Attended', '2231064.fernando@email.com', 'hqbqfjfzjjzzjepz'),
+  ('2231065', 'Deric Cahyadi', 'Not Attended', '2231065.deric@email.com', 'ktirmfdzoybvmnzd'),
+  ('2231068', 'Muhammad Arif Guntara', 'Not Attended', '2231068.muhammad@email.com', 'ahnuofyytgeutqjs'),
+  ('2231098', 'Dedy Susanto', 'Not Attended', '2231098.dedy@email.com', 'uskanhqmkedzclat'),
+  ('2231109', 'Wirianto', 'Not Attended', '2231109.wirianto@email.com', 'wpjtkmqyjsgdkiyv'),
+  ('2231127', 'Mellberg Limanda', 'Not Attended', '2231127.mellberg@email.com', 'lubjqacedhagdwtb'),
+  ('2231129', 'Christoper', 'Not Attended', '2231129.christoper@email.com', 'nyxpcneduxunvihh'),
+  ('2231004', 'Brian Tracia Bahagia', 'Not Attended', '2231004.brian@email.com', 'aswflebuuyssawod'),
+  ('2231007', 'Vincent Claudius Santoso', 'Not Attended', '2231007.vincent@email.com', 'rgqprrqsjyhpslzn'),
+  ('2231009', 'Inov Susanto', 'Not Attended', '2231009.inov@email.com', 'npfedgljmjatptpl'),
+  ('2231048', 'Paerin', 'Not Attended', '2231048.paerin@email.com', 'souuabtteyanpqqn'),
+  ('2231055', 'Risna Yunita', 'Not Attended', '2231055.risna@email.com', 'drexluacuoimvkos'),
+  ('2231059', 'Jannes Velando', 'Not Attended', '2231059.jannes@email.com', 'udjdyxjiqxnspubt'),
+  ('2231006', 'Yulsen', 'Not Attended', '2231006.yulsen@email.com', 'hlcgxmhcxehsxxro'),
+  ('2231073', 'Wilson', 'Not Attended', '2231073.wilson@email.com', 'vybqntzjbdcyzqph'),
+  ('2231086', 'Chelsea', 'Not Attended', '2231086.chelsea@email.com', 'osihzmhtpsubduny'),
+  ('2231089', 'Fransisco', 'Not Attended', '2231089.fransisco@email.com', 'giywaxjydmlrozca'),
+  ('2231105', 'Isaac Julio Herodion', 'Not Attended', '2231105.isaac@email.com', 'fwlvhcfxbqvrqydv'),
+  ('2231119', 'Rubin', 'Not Attended', '2231119.rubin@email.com', 'llusiypxidxsdsma'),
+  ('2231120', 'Kevin Gernaldi', 'Not Attended', '2231120.kevin@email.com', 'upwshrboztnuuwft'),
+  ('2231135', 'Darren Huang', 'Not Attended', '2231135.darren@email.com', 'qeifqrytwzonkolc'),
+  ('2231163', 'Stanley', 'Not Attended', '2231163.stanley@email.com', 'nqqmpffxgzpmptzj'),
+  ('2231198', 'Fajar Anugrah', 'Not Attended', '2231198.fajar@email.com', 'bbgzvaxnsinyefvt'),
+  ('2231206', 'Kelvin Tang', 'Not Attended', '2231206.kelvin@email.com', 'kmuzoqckcmsotuih'),
+  ('3170028', 'Wike Orize', 'Not Attended', '3170028.wike@email.com', 'sqmvpdexbaizokyv'),
+  ('09200101', 'Abdurrakhman Alhakim', 'Not Attended', '09200101.abdurrakhman@email.com', 'qbomdmuwxtgguzut')
+]
+
+for record in data:
+    npm, name, _, __, ___ = record
+    image = Image.new("RGB", (400, 200), color=(255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 50), f"NPM: {npm}\nName: {name}", fill=(0, 0, 0))
+    image.save(f"{output_dir}{npm}.png")
+
+for record in data:
+  cur.execute(
+      "INSERT INTO karyawan (npm, nama, status, email, token) VALUES (%s, %s, %s, %s, %s)",
+      record
+  )
+# for record in data:
+
+for record in data:
+  with open(f"{output_dir}{record[0]}.png", "rb") as img_file:
+      binary_data = img_file.read()
+      cur.execute(
+          "UPDATE karyawan SET image = %s WHERE npm = %s",
+          (binary_data, record[0])
+      )
+
+cur.execute(
+    "INSERT INTO admin (nama, password, email) VALUES (%s, %s, %s)",
+    ("admin", hashed_password.decode('utf-8'), "admin@admin.com")
+)
+# # Insert data into the table
+
+# cur.execute('INSERT INTO books (title, author, pages_num, review)'
+#             'VALUES (%s, %s, %s, %s)',
+#             ('A Tale of Two Cities',
+#              'Charles Dickens',
+#              489,
+#              'A great classic!')
+#             )
+
+
+# cur.execute('INSERT INTO books (title, author, pages_num, review)'
+#             'VALUES (%s, %s, %s, %s)',
+#             ('Anna Karenina',
+#              'Leo Tolstoy',
+#              864,
+#              'Another great classic!')
+#             )
+
+conn.commit()
+
+cur.close()
+conn.close()
